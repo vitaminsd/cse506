80c9b357b53a2d50000d9188805d79cc8c189b81
Yafei Duan <romaxduan@gmail.com>
2013-09-20 23:38:31 -0400

    added mem.h and mem.c implememting memset() and memcpy()

---
+++ include/mem.h
@@ -0,0 +1,5 @@
+#ifndef _MEM_H
+#define _MEM_H
+unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
+unsigned char *memset(unsigned char *dest, unsigned char val, int count);
+#endif
